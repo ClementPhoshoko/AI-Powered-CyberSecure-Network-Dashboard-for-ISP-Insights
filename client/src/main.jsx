b8e603cdf0dd9b0d155d09b6cdf249a67e317890
Clement Phoshoko <Clement.Phoshoko@outlook.com>
2026-06-16 13:57:29 +0200

    refactor(client): remove unused ThemeProvider import and wrapper
    remove the unnecessary ThemeProvider import and its wrapping around the root App component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './global_styles/index.css'
 import App from './App.jsx'
-import { ThemeProvider } from './context/theme_context.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>,
 )
